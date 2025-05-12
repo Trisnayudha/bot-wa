d7c4c9a87539e94a4a898ea31bffce63583b5b5f
Yudha <ingganasution@Linggas-MacBook-Air.local>
2025-05-13 03:36:48 +0700

    update

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,10 +1,12 @@
+require('dotenv').config(); // pastikan baris ini ada di atas
+
 module.exports = {
     client: 'mysql2',
     connection: {
-        host: '',
-        user: '',
-        password: '',
-        database: '',
-        port: 3306
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306
     }
 };
